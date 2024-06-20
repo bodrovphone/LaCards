6ddd6174214c489b62b97f92db88e084616742e1
oleksandr.bodrov <ext-oleksandr.bodrov@almamedia.com>
2024-06-21 00:09:49 +0300

    trying eslint alternate setUp

--- assets/styles/globalStyle.ts
+++ assets/styles/globalStyle.ts
@@ -1,4 +1,4 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 // should be used for design tokens like brand colors, spacing, typography, etc.
 export const globalStyle = StyleSheet.create({
